f856dd17a3c1f0539a9d162de3628a7926a866a1
Aditya Pareek <pareekaditya84@gmail.com>
2019-03-31 02:00:29 -0400

    updated port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(8081, function() {
+server.listen(80, function() {
   console.log(`Listening on ${server.address().port}`);
 });
 

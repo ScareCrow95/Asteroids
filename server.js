df36aa09137e434eb5ce199e503d81dccf4f0f27
Aditya Pareek <pareekaditya84@gmail.com>
2019-03-31 23:07:18 -0400

    fixed player dying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io").listen(server);
 const players = {};
-let isRed = false;
+//let isRed = false;
 
 app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
@@ -16,24 +16,25 @@ server.listen(80, function() {
 
 io.on("connection", function(socket) {
   console.log("a user connected");
-  players[socket.id] = {
-    rotation: 0,
-    x: Math.floor(Math.random() * 18) - 9,
-    y: Math.floor(Math.random() * 18) - 9,
-    playerId: socket.id,
-    team: isRed ? "red" : "blue"
-  };
-  isRed = !isRed;
-  // send the players object to the new player
-  socket.emit("currentPlayers", JSON.stringify(Object.values(players)));
-  // update all other players of the new player
-  socket.broadcast.emit("newPlayer", JSON.stringify(players[socket.id]));
+
+  socket.on("playerName", playername => {
+    players[socket.id] = {
+      rotation: 0,
+      x: Math.floor(Math.random() * 18) - 9,
+      y: Math.floor(Math.random() * 18) - 9,
+      playerId: socket.id,
+      rank: Object.keys(players).length,
+      score: 0,
+      //  team: isRed ? "red" : "blue",
+      playername: playername
+    };
+    socket.emit("currentPlayers", JSON.stringify(Object.values(players)));
+    socket.broadcast.emit("newPlayer", JSON.stringify(players[socket.id]));
+  });
 
   socket.on("disconnect", function() {
     console.log("user disconnected");
-    // remove this player from our players object
     delete players[socket.id];
-    // emit a message to all players to remove this player
     io.emit("playerDisconnect", socket.id);
   });
 
@@ -42,11 +43,19 @@ io.on("connection", function(socket) {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
     socket.broadcast.emit("playerMovement", JSON.stringify(players[socket.id]));
   });
 
-  socket.on("fire", movementData => {
-    socket.broadcast.emit("fire", movementData);
+  socket.on("fire", data => {
+    console.log("fire : " + data);
+
+    socket.broadcast.emit("fire", data);
+  });
+
+  socket.on("playerHit", data => {
+    console.log("hit : " + data);
+
+    delete players[JSON.parse(data).playerId];
+    io.emit("playerHit", data);
   });
 });
